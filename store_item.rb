2d31b9e2c59ff982c4dfc5d376e45c7563ecc66b
Andrew Wei <andrew.wei92@yahoo.com>
2016-04-04 20:38:50 -0700

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,20 @@
+hats = [
+  {
+    color: "blue",
+    size: "small",
+    price: 20
+  },
+  {
+    :color => "red",
+    size: "medium",
+    price: 30
+  },
+  {
+    color: "green",
+    size: "large",
+    price: 35
+  }
+]
+
+p hats
+puts hats[0][:color]
